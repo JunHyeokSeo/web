960ec4e22874aec6a988980355d7c5743bf10203
JunHyeok Seo <sjhgd107@naver.com>
2023-09-15 17:19:31 +0900

    feat: jQuery 실습

---
+++ web/jQuery/work/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    //id 값이 sample1으로 되어있는 Element를 구해온다
+    $('#sample1').hide();
+
+    //선택자로 Element를 구해온다.
+    $('div').hide();
+
+    //jQuery 함수의 역할은 하나, DOM Element를 불러온다
+});
